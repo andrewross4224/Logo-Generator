df11be90171225db0d60b171f60a02a4a77e8b7b
Andrew Ross <andrew.ross4224@gmail.com>
2023-11-02 15:01:16 -0400

    changed structure of classes and added svg item

--- library/shapes.js
+++ library/shapes.js
@@ -7,23 +7,23 @@ class Shape {
 }
 
 class Triangle extends Shape {
-    constructor(text, textColor, shapeColor, type){
+    constructor(text, textColor, shapeColor){
         super(text, textColor, shapeColor)
-        this.type = type
+        this.svg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><polygon points="150 200, 300 20, 0 20" fill="${this.shapeColor}"/><text x="150" y="100" font-size="65" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`
     }
 }
 
 class Circle extends Shape {
-    constructor(text, textColor, shapeColor, type){
+    constructor(text, textColor, shapeColor){
         super(text, textColor, shapeColor)
-        this.type = type
+        this.svg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><circle cx="150" cy="100" r="95" fill="${this.shapeColor}"/><text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`
     }
 }
 
 class Square extends Shape {
-    constructor(text, textColor, shapeColor, type){
+    constructor(text, textColor, shapeColor){
         super(text, textColor, shapeColor)
-        this.type = type
+        this.svg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"><polygon points="40 180, 260 180, 260 20,  40 20" fill="${this.shapeColor}"/><text x="150" y="130" font-size="80" text-anchor="middle" fill="${this.textColor}">${this.text}</text></svg>`
     }
 }
 
